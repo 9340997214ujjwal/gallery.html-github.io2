543c884c3c05f979674a21ce3a00d55d91201554
9340997214ujjwal <94543596+9340997214ujjwal@users.noreply.github.com>
2022-02-06 02:02:26 +0530

    uplode

---
+++ gallery.js
@@ -0,0 +1,29 @@
+const API_KEY = "7jU9WtqNhEfVMVwvYOxmTLeqbLal3gLE9wwQ-W53o7M";
+const API_SECRET = "7txsPHXA7V9HolUEmdNc1-OKiFQ97Kodt8-yE0iaTTE";
+
+let search_param = "orange";
+fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${search_param}&per_page=30`)
+.then(function(res) {
+    return res.json();
+})
+.then(function(res) {
+    console.log(res.results.length);
+    for(let i = 0; i < res.results.length; i = i + 1) {
+        let single_result = res.results[i];
+        let single_url = single_result.urls.regular;
+        console.log(single_url);
+    }
+})
+searchBox.oninput = () =>{
+   images.forEach(hide => hide.style.display = 'none');
+   let value = searchBox.value;
+   images.forEach(filter =>{
+      let title = filter.getAttribute('data-title');
+      if(value == title){
+         filter.style.display = 'block';
+      }
+      if(searchBox.value == ''){
+         filter.style.display = 'block';
+      }
+   });
+};
